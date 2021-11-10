6433e65ce387d91edb85bf3d616aaf7288f2d9b0
webitall <welcome4899@gmail.com>
2021-11-10 19:56:04 +0200

    basic slider animation done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,23 +7,3 @@ $(document).ready(function () {
     adaptiveHeight: true,
   });
 });
-
-const sliderItem = document.querySelectorAll(".slider__item").length / 4;
-const counter = document.querySelector(".slides-counter");
-const prevBtn = document.querySelector(".fa-chevron-left");
-const nextBtn = document.querySelector(".fa-chevron-right");
-// let current = sliderItem % 2;
-// counter.textContent = current + "/" + `${sliderItem}`;
-
-// prevBtn.addEventListener("click", () => {
-//     console.log(prevBtn);
-//     sliderItem = -1;
-
-//   counter.textConten = current + "/" + sliderItem;
-// });
-
-// if (current == sliderItem - 1) {
-//   current = 2;
-// } else if (current == sliderItem) {
-//   current = sliderItem;
-// }
