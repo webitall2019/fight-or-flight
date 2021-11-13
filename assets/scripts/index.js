dda1ef07d32d84d1e8a4c3288358afcd204cac75
webitall <welcome4899@gmail.com>
2021-11-13 18:48:48 +0200

    ready to review

--- assets/scripts/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(".fof__slider").slick({
-    slidesToShow: 4,
-    autoPlay: true,
-    prevArrow: '<i class="fas fa-chevron-left"></i>',
-    nextArrow: '<i class="fas fa-chevron-right"></i>',
-    adaptiveHeight: true,
-  });
-});
